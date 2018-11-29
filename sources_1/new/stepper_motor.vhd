57b995c8a59ed9c37c4140b2a1c0e526e0a6db1a
Jason Ngu <jason.ngu99@gmail.com>
2018-11-28 16:17:31 -0800

    Added stepper motor file

---
+++ sources_1/new/stepper_motor.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/28/2018 04:14:55 PM
+-- Design Name: 
+-- Module Name: stepper_motor - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity stepper_motor is
+--  Port ( );
+end stepper_motor;
+
+architecture Behavioral of stepper_motor is
+
+begin
+
+
+end Behavioral;

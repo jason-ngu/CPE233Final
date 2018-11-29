f5ddd738fc24ee47e36574929c78dcc76216a494
Jason Ngu <jason.ngu99@gmail.com>
2018-11-28 17:57:38 -0800

    Added actually working code

--- sources_1/new/stepper_motor.vhd
+++ sources_1/new/stepper_motor.vhd
@@ -1,43 +1,57 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/28/2018 04:14:55 PM
--- Design Name: 
--- Module Name: stepper_motor - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity stepper_motor is
---  Port ( );
+  Port ( DIN : in std_logic_vector(7 downto 0);
+         CLK : in std_logic;
+         STEP : out std_logic;
+         DIR : out std_logic);
 end stepper_motor;
 
 architecture Behavioral of stepper_motor is
 
+signal prev_din : std_logic_vector (7 downto 0);
+signal current_din : std_logic_vector ( 7 downto 0);
+signal data_changed : std_logic;
+signal enable : std_logic;
+signal count : integer := 0;
+
 begin
+
+current_din <= DIN;
+
+reg: process(CLK)
+begin
+    if rising_edge(CLK) then
+        prev_din <= current_din;
+    end if;
+end process;
+
+trigger_change: process(prev_din, current_din)
+begin
+    if prev_din = current_din then
+        data_changed <= '0';
+    else
+        data_changed <= '1';
+    end if;
+end process;
+
+on_change: process(data_changed, CLK)
+begin
+    if rising_edge(data_changed) then
+        count <= 0;
+    end if;
+    
+    if rising_edge(CLK) then
+        if count < to_integer(unsigned(current_din)) then
+            count <= count + 1;
+            enable <= '1';
+        else
+            enable <= '0';
+        end if;
+    end if;
+end process;
 
 
 end Behavioral;

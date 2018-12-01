654d68b2919071f11c8fbdd48f5c7f8927136026
Jason Ngu <jason.ngu99@gmail.com>
2018-11-30 16:58:44 -0800

    Added output process and scaled clock component

--- sources_1/new/stepper_motor.vhd
+++ sources_1/new/stepper_motor.vhd
@@ -6,11 +6,21 @@ entity stepper_motor is
   Port ( DIN : in std_logic_vector(7 downto 0);
          CLK : in std_logic;
          STEP : out std_logic;
-         DIR : out std_logic);
+         DIR : out std_logic;
+         data_changed_debug : out std_logic;
+         enable_debug : out std_logic);
 end stepper_motor;
 
 architecture Behavioral of stepper_motor is
 
+component scale_clock is
+  port (
+    clk_50Mhz : in  std_logic;
+    rst       : in  std_logic;
+    clk_2Hz   : out std_logic);
+end component scale_clock;
+
+signal clk_sig : std_logic;
 signal prev_din : std_logic_vector (7 downto 0);
 signal current_din : std_logic_vector ( 7 downto 0);
 signal data_changed : std_logic;
@@ -23,9 +33,12 @@ current_din <= DIN;
 
 DIR <= current_din(7);
 
-reg: process(CLK)
+data_changed_debug <= data_changed;
+enable_debug <= enable;
+
+reg: process(clk_sig)
 begin
-    if rising_edge(CLK) then
+    if rising_edge(clk_sig) then
         prev_din <= current_din;
     end if;
 end process;
@@ -39,13 +52,13 @@ begin
     end if;
 end process;
 
-on_change: process(data_changed, CLK)
+on_change: process(data_changed, clk_sig)
 begin
-    if rising_edge(data_changed) then
+    if (data_changed = '1') then
         count <= 0;
-    end if;
     
-    if rising_edge(CLK) then
+    elsif rising_edge(clk_sig) then
+    
         if count < to_integer(unsigned(current_din)) then
             count <= count + 1;
             enable <= '1';
@@ -55,15 +68,18 @@ begin
     end if;
 end process;
 
-set_out: process(enable,CLK)
+set_out: process(enable,clk_sig)
 begin
     if enable = '1' then
-        STEP <= CLK;
+        STEP <= clk_sig;
     else
         STEP <= '0';
     end if;
 end process;
 
-
+clock : scale_clock port map(
+        clk_50Mhz => clk,
+        rst => '0',     
+        clk_2Hz => clk_sig);
 
 end Behavioral;

268bbecfa55e7b34911893fa598e2397053cdcfe
Jason Ngu <jason.ngu99@gmail.com>
2018-12-04 22:33:28 -0800

    Added extra output
    Outputs 0 before outputting the proper value to stepper1/2

--- Final.asm
+++ Final.asm
@@ -8,9 +8,10 @@
 
 INIT:	IN R0, INPUT_STEP ; Stores amount of steps
 		IN R1, INPUT_DIR ; Determines left/right/up/down
+		MOV R2, 0x00
 
-REMOVE_MSB:	MOV R2, R0 ; Copy STEP to new register for manipulation
-			CMP R2, 0x80 ; Check if greater than 128 to remove 8th bit if necessary
+REMOVE_MSB:	MOV R3, R0 ; Copy STEP to new register for manipulation
+			CMP R3, 0x80 ; Check if greater than 128 to remove 8th bit if necessary
 			BRCC SUBTRACT
 
 CHECK_DIR: 	CMP R1, 0x00	; Based on direction, goes to proper output subroutine
@@ -23,25 +24,29 @@ CHECK_DIR: 	CMP R1, 0x00	; Based on direction, goes to proper output subroutine
 			BREQ DOWN
 			BRN END			; If direction input is not left/right/up/down, nothing is outputted
 
-SUBTRACT:	SUB R2, 0x80
+SUBTRACT:	SUB R3, 0x80
 			BRN CHECK_DIR
 
 ; Direction 0 on STEPPER1 -> DIR is 0
 RIGHT:	OUT R2, STEPPER1
+		OUT R3, STEPPER1
 		BRN END
 
 ; Direction 1 on STEPPER1 -> DIR is 1
-LEFT:	ADD R2, 0x80
+LEFT:	ADD R3, 0x80
 		OUT R2, STEPPER1
+		OUT R3, STEPPER1
 		BRN END
 
 ; Direction 0 on STEPPER2 -> DIR is 2
 UP:		OUT R2, STEPPER2
+		OUT R3, STEPPER2
 		BRN END
 
 ; Direction 1 on STEPPER2 -> DIR is 3
-DOWN:	ADD R2, 0x80
+DOWN:	ADD R3, 0x80
 		OUT R2, STEPPER2
+		OUT R3, STEPPER2
 		BRN END
 
 END: 
